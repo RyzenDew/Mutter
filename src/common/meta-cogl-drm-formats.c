94b000b1fda9c4f4fd46ecf4368052ebfe0c1aa2
RyzenDew <84041391+RyzenDew@users.noreply.github.com>
2024-02-14 12:56:42 -0400

    hmm
    yup

---
+++ src/common/meta-cogl-drm-formats.c
@@ -0,0 +1,89 @@
+/* meta-cogl-drm-formats.c
+ *
+ * Copyright (C) 2020 Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
+ * Copyright (C) 2023 Collabora Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ */
+
+#include "config.h"
+
+#include "common/meta-cogl-drm-formats.h"
+
+
+/**
+ * meta_drm_format_to_string:
+ * @tmp: temporary buffer
+ * @drm_format: DRM fourcc pixel format
+ *
+ * Returns a pointer to a string naming the given pixel format,
+ * usually a pointer to the temporary buffer but not always.
+ * Invalid formats may return nonsense names.
+ *
+ * When calling this, allocate one MetaDrmFormatBuf on the stack to
+ * be used as the temporary buffer.
+ */
+const char *
+meta_drm_format_to_string (MetaDrmFormatBuf *tmp,
+                           uint32_t          drm_format)
+{
+  int i;
+
+  if (drm_format == DRM_FORMAT_INVALID)
+    return "INVALID";
+
+  G_STATIC_ASSERT (sizeof (tmp->s) == 5);
+  for (i = 0; i < 4; i++)
+    {
+      char c = (drm_format >> (i * 8)) & 0xff;
+      tmp->s[i] = g_ascii_isgraph (c) ? c : '.';
+    }
+
+  tmp->s[i] = 0;
+
+  return tmp->s;
+}
+
+const MetaFormatInfo *
+meta_format_info_from_drm_format (uint32_t drm_format)
+{
+  const size_t n = G_N_ELEMENTS (meta_format_info);
+  size_t i;
+
+  for (i = 0; i < n; i++)
+    {
+      if (meta_format_info[i].drm_format == drm_format)
+        return &meta_format_info[i];
+    }
+
+  return NULL;
+}
+
+const MetaFormatInfo *
+meta_format_info_from_cogl_format (CoglPixelFormat cogl_format)
+{
+  const size_t n = G_N_ELEMENTS (meta_format_info);
+  size_t i;
+
+  for (i = 0; i < n; i++)
+    {
+      if (meta_format_info[i].cogl_format == cogl_format)
+        return &meta_format_info[i];
+    }
+
+  return NULL;
+}
